48f834e09e44c24ecfd50de941e007e2770a8850
nakano16180 <36945685+nakano16180@users.noreply.github.com>
2026-02-20 17:58:52 +0900

    Add EventBridge scheduled Lambda sample

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { HitCounter } from './hitcounter';
 
 export class CdkWorkshopStack extends cdk.Stack {
@@ -10,16 +12,28 @@ export class CdkWorkshopStack extends cdk.Stack {
     const hello = new lambda.Function(this, 'HelloHandler', {
       runtime: lambda.Runtime.NODEJS_18_X,
       code: lambda.Code.fromAsset('lambda'),
-      handler: 'hello.handler'
+      handler: 'hello.handler',
     });
 
     const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
-      downstream: hello
+      downstream: hello,
     });
 
     // defines an API Gateway REST API resource backed by our "hello" function.
     new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: helloWithCounter.handler
+      handler: helloWithCounter.handler,
+    });
+
+    const eventBridgeHandler = new lambda.Function(this, 'EventBridgeHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'eventbridge.handler',
+    });
+
+    new events.Rule(this, 'EventBridgeSampleRule', {
+      description: 'Sample EventBridge rule that invokes a Lambda function every 5 minutes',
+      schedule: events.Schedule.rate(cdk.Duration.minutes(5)),
+      targets: [new targets.LambdaFunction(eventBridgeHandler)],
     });
   }
 }
